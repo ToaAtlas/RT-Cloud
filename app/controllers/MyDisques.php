5cb119eb3e837a56e2538199ff7bfefbaf22896d
François <21504921@etu.unicaen.fr>
2016-04-12 15:39:21 +0200

    todo 1.1
    en cours

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -10,6 +10,29 @@ class MyDisques extends Controller{
 	}
 	public function index() {
 		echo Jquery::compile();
+		if (Auth::isAuth()){ //verifie user connecté
+			$user = Auth::getUser(); // get user name
+			var_dump($user); // c'est un objet
+			echo"<br><br>";
+			$userId = $user->getId(); // on recup l'id user
+			$disque = \micro\orm\DAO::getAll("disque", "idUtilisateur=$userId");
+			var_dump($disque); // on recup les disques du user
+			echo"<br><br>";
+			$numDisque = 1;
+			foreach ($disque as $disque){ // on les affiche
+				echo "disque numéro $numDisque<br>";
+				echo $disque->getSize()." taille";
+				echo "<br>";
+				echo $disque->getOccupation()." occupation";
+				echo "<br>";
+				echo $disque->getQuota().' quota';
+				echo "<br>";
+				$numDisque++;
+			}
+		}
+		else {
+			echo "<div class='content'><h4>Veuillez vous connecter</h4></div>";
+		}
 	}
 
 	public function finalize(){
