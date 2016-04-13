f8590a3331134c3a6df7274710e52ea966c776f3
dozGrou <dodo800882@gmail.com>
2016-04-13 21:00:49 +0200

    Occupation disques

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -2,6 +2,8 @@
 use micro\controllers\Controller;
 use micro\js\Jquery;
 use micro\utils\RequestUtils;
+use micro\orm\DAO;
+
 class MyDisques extends Controller{
 	public function initialize(){
 		if(!RequestUtils::isAjax()){
@@ -14,6 +16,10 @@ class MyDisques extends Controller{
 			$user = Auth::getUser(); // get user name		
 			$userId = $user->getId(); // on recup l'id user
 			$disques = \micro\orm\DAO::getAll("disque", "idUtilisateur=$userId");// on recup les disques du user, tableau d'objet
+			foreach($disques as $disque)
+				$disque->occupation = DirectoryUtils::formatBytes($disque->getOccupation() / 100 * $disque->getQuota());
+
+
 			$this->loadView("MyDisques/index.html", array("user"=>$user,"disques"=>$disques));
 		}
 		else {
