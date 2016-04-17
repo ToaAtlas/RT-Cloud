98efb239d0f34077f215bf557818fbdc9ba992e4
dozGrou <dodo800882@gmail.com>
2016-04-17 17:16:03 +0200

    Update

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -3,6 +3,19 @@ use micro\orm\DAO;
 class Admin extends \BaseController {
 
 	public function index() {
-		$this->loadView('Admin/index.html');
+		$count = (object)[];
+		$count->all = (object)[];
+		$count->today = (object)[];
+
+		$count->all->user = DAO::count('utilisateur');
+		$count->all->disk = DAO::count('disque');
+		$count->all->tarif = DAO::count('tarif');
+		$count->all->service = DAO::count('service');
+
+		$count->today->user = DAO::count('utilisateur', 'DAY(createdAt) = DAY(NOW())');
+		$count->today->disk = DAO::count('disque', 'DAY(createdAt) = DAY(NOW())');
+
+
+		$this->loadView('Admin/index.html', ['count' => $count]);
 	}
 }
